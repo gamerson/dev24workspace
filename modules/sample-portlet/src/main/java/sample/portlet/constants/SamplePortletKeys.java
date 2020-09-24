feda83c0dfb327fb7dff4380b4f82eedf8f91072
Gregory Amerson <gregory.amerson@liferay.com>
2020-09-24 15:23:57 -0500

    old style portlet

---
+++ modules/sample-portlet/src/main/java/sample/portlet/constants/SamplePortletKeys.java
@@ -0,0 +1,11 @@
+package sample.portlet.constants;
+
+/**
+ * @author greg
+ */
+public class SamplePortletKeys {
+
+	public static final String SAMPLE =
+		"sample_portlet_SamplePortlet";
+
+}
---
+++ modules/sample-portlet/src/main/java/sample/portlet/portlet/SamplePortlet.java
@@ -0,0 +1,30 @@
+package sample.portlet.portlet;
+
+import sample.portlet.constants.SamplePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author greg
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Sample",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + SamplePortletKeys.SAMPLE,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class SamplePortlet extends MVCPortlet {
+}
