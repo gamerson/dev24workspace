d0ee30d8a140dfa31ce81d23cb068191c1f7800c
Gregory Amerson <gregory.amerson@liferay.com>
2020-09-24 14:35:40 -0500

    add sample web

---
+++ modules/sample/sample-web/src/main/java/sample/web/constants/SampleWebPortletKeys.java
@@ -0,0 +1,11 @@
+package sample.web.constants;
+
+/**
+ * @author greg
+ */
+public class SampleWebPortletKeys {
+
+	public static final String SAMPLEWEB =
+		"sample_web_SampleWebPortlet";
+
+}
---
+++ modules/sample/sample-web/src/main/java/sample/web/portlet/SampleWebPortlet.java
@@ -0,0 +1,30 @@
+package sample.web.portlet;
+
+import sample.web.constants.SampleWebPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author greg
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=SampleWeb",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + SampleWebPortletKeys.SAMPLEWEB,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class SampleWebPortlet extends MVCPortlet {
+}
